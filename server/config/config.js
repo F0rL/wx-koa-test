d6ab6162056ba59edfceb5311ce7e5079dca8040
F0rl <F0rL@users.noreply.github.com>
2019-09-27 22:38:38 +0800

    wxPage init :tada:

--- server/config/config.js
+++ server/config/config.js
@@ -13,8 +13,8 @@ module.exports = {
     expiresIn: 60*60*24
   },
   wx: {
-    appId: 'wx2826815a3e7395f8',
-    appSecret: 'b73f0c2c2a788e4bbc2314dae92ef75c',
+    appId: 'wxf1b3b963d165cef2',
+    appSecret: '9fa8efff7637fd226cf3e0b665b4c486',
     loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
   }
 }
