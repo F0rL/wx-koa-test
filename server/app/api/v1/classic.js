9f1db97d05857147644ac01510849687c370e6d6
F0rl <F0rL@users.noreply.github.com>
2019-10-03 14:42:30 +0800

    完善课获取最新期刊

--- server/app/api/v1/classic.js
+++ server/app/api/v1/classic.js
@@ -3,6 +3,7 @@ const Router = require("koa-router");
 const {Auth} = require('../../../middlewares/auth')
 const {Flow} = require('../../models/flow')
 const {Art} = require('../../models/art')
+const {Favor} = require('../../models/favor')
 
 const router = new Router({
   prefix: '/v1/classic'
@@ -20,8 +21,10 @@ router.get("/lastest", new Auth().m, async(ctx, next)=>{
     ]
   })
   const art = await Art.getData(flow.art_id, flow.type)
+  const likeLatest = await Favor.userLikeIt(flow.art_id, flow.type, ctx.auth.uid)
   // art是一个类，对象序列化json
   art.setDataValue('index', flow.index)
+  art.setDataValue('like_status', likeLatest)
   // 下面的方法不严谨
   // art.dataValues.index = flow.index
   ctx.body = art
