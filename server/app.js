5bd21567079353f22e514aae7077a7a3f63134cc
F0rl <F0rL@users.noreply.github.com>
2019-10-06 01:23:22 +0800

    静态资源说明

--- server/app.js
+++ server/app.js
@@ -17,3 +17,13 @@ InitManager.initCore(app)
 // app.use(router.routes()).use(router.allowedMethods());
 const port = process.env.PORT || 3000;
 app.listen(port);
+
+// 静态资源 图片等 消耗流量
+// 1.网站目录
+// 2.静态资源服务器 微服务 带宽要足够
+// 3.云服务 贵  ECS RDS OSS  能够CDN
+// 4.github等免费静态资源
+
+// js css html某些情况下可以是静态资源
+// vue/react 前端单页面 SEO困难 还是做后台CMS webApp
+// to C 还是用模板 SSR
